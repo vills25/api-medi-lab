7b677f21a4d15fc16d5bde1254be70a94c3806f9
vills25 <132877214+vills25@users.noreply.github.com>
2024-01-26 15:02:33 +0530

    #

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t5sjr+x5%at6a47l*)s3fr0j*w68_c=*5n@*s5k(^&dh(f$h-3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
